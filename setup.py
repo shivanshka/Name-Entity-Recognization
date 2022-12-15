6416a78210caa1944f6601605ae97601f6b022ee
Shivansh Kaushal <kaushal.shivansh630@gmail.com>
2022-12-15 23:29:24 +0530

    setup.py updated

--- setup.py
+++ setup.py
@@ -4,8 +4,10 @@ with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
 ## edit below variables as per your requirements -
-REPO_NAME = "REPO_NAME"
-AUTHOR_USER_NAME = "c17hawke"
+REPO_NAME = "Name-Entity_Recognization"
+AUTHOR_NAME = "Shivansh Kaushal"
+AUTHOR_USER_NAME = "shivanshka"
+EMAIL = "kaushal.shivansh630@gmail.com"
 SRC_REPO = "src"
 LIST_OF_REQUIREMENTS = []
 
@@ -13,12 +15,12 @@ LIST_OF_REQUIREMENTS = []
 setup(
     name=SRC_REPO,
     version="0.0.1",
-    author=AUTHOR_USER_NAME,
-    description="A small package for DVC",
+    author=AUTHOR_NAME,
+    description="NER NLP project",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
-    author_email="sunny.c17hawke@gmail.com",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}.git",
+    author_email=EMAIL,
     packages=[SRC_REPO],
     license="MIT",
     python_requires=">=3.6",
